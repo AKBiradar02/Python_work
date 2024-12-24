70b29e143d82375e5c298ec547f337b84ec217fc
Biradar Abhay Kapil <1032211780@mitwpu.edu.in>
2024-12-24 22:38:34 +0530

    Update email_slicer.py

--- projects/email_slicer.py
+++ projects/email_slicer.py
@@ -1,12 +1,13 @@
-#Email Slicer used for detecting email and the domain with email id.
+#Email Slicer used for detecting email and the domain with email 
+#email slicer
+#get user email id from user input
 
-
-#user input
-email = input("Enter your Email: ").strip()
-
-#get username and domain
-username = email[:email.index('@')]
-domain = email[email.index('@') + 1:]
-
-print(f"Your username is {username} & domain is {domain})
-
+email = input("Enter your email:").strip()
+#get user name
+user_name = email[:email.index("@")]
+#get domain name
+domain_name = email[email.index("@")+1:]
+#get formatted output
+output = "Your user name is '{}' and your domain name is '{}'".format(user_name,domain_name)
+#print output
+print(output)
